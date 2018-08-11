a346213dbdd18c8acdbdf388ad4d434348bb2b67
Dominic Burkart <1dominicb@gmail.com>
2018-08-11 16:29:47 -0400

    add c function

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,13 @@ impl<T> HeapFn<T> {
             f: Arc::new(b),
         }
     }
+
+    pub fn c(&self) -> Arc<Box<T>> {
+        (self.f.clone())
+    }
 }
+
+
 
 #[macro_export]
 macro_rules! heap_fn {
@@ -21,17 +27,27 @@ macro_rules! heap_fn {
     ( $f:expr ) => { $crate::HeapFn::new(Box::new($f)) };
 }
 
+
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn zero_arity_heap_fn() {
         let wrapped_prim = |i: u32| {println!("{}", i + 10)};
         let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
 
-        (heap_fn!(||{println!("Neat!")}).f)();
-        (heap_fn!(||{wrapped_prim(10)}).f)();
-        (heap_fn!(||{wrapped_vec(vec!["nice", "cool"])}).f)();
+        heap_fn!(||{println!("Neat!")}).c();
+        heap_fn!(||{wrapped_prim(10)}).c();
+        heap_fn!(||{wrapped_vec(vec!["nice", "cool"])}).c();
+    }
+
+    #[test]
+    fn one_arity_heap_fn() {
+        let wrapped_prim = |i: u32| {println!("{}", i + 10)};
+        let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
+
+        heap_fn!(|a: &str|{println!("{}", a)}).c()("Awesome!");
+        heap_fn!(|i: u32| {println!("{}", i + 10)}).c()(10);
+        heap_fn!(|v: Vec<&str>| { println!("{:?}", v) }).c()(vec!["nice", "cool"]);
     }
 
 }
