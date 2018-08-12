3c7b72b183c92dfc4c3ccaee75c83c1b8d2df323
Dominic Burkart <1dominicb@gmail.com>
2018-08-12 17:13:15 -0400

    impl macros, HeapFn to BoxFn
    spawn_fn macro -> starts a new thread when given a closure and input, moving only when necessary.
    heap_fn macro -> box_fn
    rc macro -> boxes input
    arc macro -> Arc to Box of input
    
    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,125 @@
 use std::sync::Arc;
-
-pub type HeapFn<T> = Arc<Box<T>>;
+use std::thread;
 
 #[macro_export]
-macro_rules! heap_fn {
+macro_rules! rc {
+    ( $f:ident ) => { Box::new($f) };
+    ( $f:expr ) => { Box::new($f) };
+}
+
+#[macro_export]
+macro_rules! arc {
     ( $f:ident ) => { $crate::Arc::new(Box::new($f)) };
     ( $f:expr ) => { $crate::Arc::new(Box::new($f)) };
 }
 
+#[macro_export]
+macro_rules! box_fn {
+    ( $f:ident ) => { rc!($f) };
+    ( $f:expr ) => { rc!($f) };
+}
+
+#[macro_export]
+macro_rules! spawn_fn {
+    ( $f:expr ) => { $crate::thread::spawn($f)};
+    ( $f:ident, $( $arg:ident ),* ) => { {let v = $f.clone(); $crate::thread::spawn(move || {v($($arg),*)})}};
+    ( $f:expr, $( $arg:expr ),* ) => { $crate::thread::spawn(move || {$f($($arg),*)}) };
+}
+
+pub type BoxFn<T> = Box<T>;
 
 #[cfg(test)]
 mod tests {
-    use HeapFn;
+    use super::*;
 
     #[test]
-    fn zero_arity_heap_fn() {
+    fn zero_arity_box_fn() {
         let wrapped_prim = |i: u32| { println!("{}", i + 10) };
         let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
 
-        let _t = heap_fn!(||{println!("Neat!")});
-        let _c = heap_fn!(||{println!("Neat!")});
+        let _t = box_fn!(||{println!("Neat!")});
+        let _c = box_fn!(||{println!("Neat!")});
 
-        heap_fn!(||{println!("Neat!")})();
-        heap_fn!(||{wrapped_prim(10)})();
-        heap_fn!(||{wrapped_vec(vec!["nice", "cool"])})();
+        box_fn!(||{println!("Neat!")})();
+        box_fn!(||{wrapped_prim(10)})();
+        box_fn!(||{wrapped_vec(vec!["nice", "cool"])})();
     }
 
     #[test]
-    fn one_arity_heap_fn() {
-        heap_fn!(|a: &str|{println!("{}", a)})("Awesome!");
-        heap_fn!(|i: u32| {println!("{}", i + 10)})(10);
-        heap_fn!(|v: Vec<&str>| { println!("{:?}", v) })(vec!["nice", "cool"]);
+    fn one_arity_box_fn() {
+        box_fn!(|a: &str|{println!("{}", a)})("Awesome!");
+        box_fn!(|i: u32| {println!("{}", i + 10)})(10);
+        box_fn!(|v: Vec<&str>| { println!("{:?}", v) })(vec!["nice", "cool"]);
     }
 
     #[test]
-    fn pass_let_exp_heap_fn() {
+    fn pass_named_box_fn() {
         let wrapped_prim = |i: u32| { println!("{}", i + 10) };
         let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
 
-        heap_fn!(wrapped_prim)(10);
-        heap_fn!(wrapped_vec)(vec!["nice", "cool"])
+        box_fn!(wrapped_prim)(10);
+        box_fn!(wrapped_vec)(vec!["nice", "cool"])
     }
 
     #[test]
-    fn pass_function_heap_fn() {
+    fn pass_function_box_fn() {
         fn func(s: &str) { println!("{}", s)}
 
-        heap_fn!(func)("Wow!");
+        box_fn!(func)("Wow!");
     }
 
     #[test]
-    fn in_struct_heap_fn() {
-        type T = HeapFn<Fn(&str) -> String>;
+    fn in_struct_box_fn() {
+        type T = BoxFn<Fn(&str) -> String>;
 
         struct F {
             c: T
         }
 
-        let c: T = heap_fn!(|s: &str| -> String {s.to_string()});
+        let c: T = box_fn!(|s: &str| -> String {s.to_string()});
 
         let mut f = F { c };
 
-        f.c = heap_fn!(
+        f.c = box_fn!(
             |d: &str| -> String {"reassign once".to_string()}
         );
 
-        f.c = heap_fn!(
-            |d: &str| -> String {"and again".to_string()}
-        )
+        f.c = box_fn!(
+            |_: &str| {"and again".to_string()}
+        );
+    }
+
+    #[test]
+    fn multithreading_box_fn() {
+        let eg = box_fn!(|x: i32| -> i32 {x + 2});
+
+        let mut v1 = Vec::new();
+        for i in 0..10000 {
+            let cl = eg.clone();
+            v1.push(thread::spawn(move ||{cl(i)})); // move is necessary
+            //v2.push(spawn_fn!(eg, i));
+        }
+
+        for res in v1.into_iter() {
+            res.join();
+        }
+    }
+
+    #[test]
+    fn multithreading_spawn_fn() {
+        let eg = box_fn!(|x: i32| -> i32 {x + 2});
+        let also = box_fn!(|x: i32, y: i32| -> i32 {x + y});
+
+        let mut v1 = Vec::new();
+        for i1 in 0..10000 {
+            let i2 = i1 + 10;
+            v1.push(spawn_fn!(eg, i1));
+            v1.push(spawn_fn!(also, i1, i2)); // woohoo multi-arity!
+        }
+        v1.push(spawn_fn!(||{println!("accepts closures to run in their own thread!"); 1}));
+
+        for res in v1.into_iter() {
+            res.join();
+        }
     }
 }
