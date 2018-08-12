4bed919436e7186c5b43486a4458f1518a057aa6
Dominic Burkart <1dominicb@gmail.com>
2018-08-11 21:57:35 -0400

    let arc do its job tbh!

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,51 @@
 use std::sync::Arc;
 
-
-#[derive(Debug, Clone, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub struct HeapFn<T>{
-    f: Arc<Box<T>>
-}
-
-impl<T> HeapFn<T> {
-    pub fn new(b: Box<T>) -> Self {
-        HeapFn {
-            f: Arc::new(b),
-        }
-    }
-
-    pub fn c(&self) -> Arc<Box<T>> {
-        (self.f.clone())
-    }
-}
-
-
+pub type HeapFn<T> = Arc<Box<T>>;
 
 #[macro_export]
 macro_rules! heap_fn {
-    ( $f:ident ) => { $crate::HeapFn::new(Box::new($f)) };
-    ( $f:expr ) => { $crate::HeapFn::new(Box::new($f)) };
+    ( $f:ident ) => { $crate::Arc::new(Box::new($f)) };
+    ( $f:expr ) => { $crate::Arc::new(Box::new($f)) };
 }
 
 
 #[cfg(test)]
 mod tests {
+    use HeapFn;
+
+    fn func(s: &str) { println!("{}", s)}
+
     #[test]
     fn zero_arity_heap_fn() {
-        let wrapped_prim = |i: u32| {println!("{}", i + 10)};
+        let wrapped_prim = |i: u32| { println!("{}", i + 10) };
         let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
 
-        heap_fn!(||{println!("Neat!")}).c();
-        heap_fn!(||{wrapped_prim(10)}).c();
-        heap_fn!(||{wrapped_vec(vec!["nice", "cool"])}).c();
+        let _t = heap_fn!(||{println!("Neat!")});
+        let _c = heap_fn!(||{println!("Neat!")});
+
+        heap_fn!(||{println!("Neat!")})();
+        heap_fn!(||{wrapped_prim(10)})();
+        heap_fn!(||{wrapped_vec(vec!["nice", "cool"])})();
     }
 
     #[test]
     fn one_arity_heap_fn() {
-        heap_fn!(|a: &str|{println!("{}", a)}).c()("Awesome!");
-        heap_fn!(|i: u32| {println!("{}", i + 10)}).c()(10);
-        heap_fn!(|v: Vec<&str>| { println!("{:?}", v) }).c()(vec!["nice", "cool"]);
+        heap_fn!(|a: &str|{println!("{}", a)})("Awesome!");
+        heap_fn!(|i: u32| {println!("{}", i + 10)})(10);
+        heap_fn!(|v: Vec<&str>| { println!("{:?}", v) })(vec!["nice", "cool"]);
     }
 
     #[test]
-    fn pass_let_exp(){
-        let wrapped_prim = |i: u32| {println!("{}", i + 10)};
+    fn pass_let_exp_heap_fn() {
+        let wrapped_prim = |i: u32| { println!("{}", i + 10) };
         let wrapped_vec = |v: Vec<&str>| { println!("{:?}", v) };
 
-        heap_fn!(wrapped_prim).c()(10);
-        heap_fn!(wrapped_vec).c()(vec!["nice", "cool"])
+        heap_fn!(wrapped_prim)(10);
+        heap_fn!(wrapped_vec)(vec!["nice", "cool"])
+    }
+
+    #[test]
+    fn pass_function_heap_fn() {
+        heap_fn!(func)("Wow!");
     }
 }
